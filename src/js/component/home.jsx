f67af19801619a344a85c15e7886010917895dd9
Jimmbo19 <jega190112@gmail.com>
2024-08-26 03:01:19 +0000

    Jumbotron Top

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,12 +4,13 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 import NAV from "../../react_doc/nav_react";
-
+import JumTop from "../../react_doc/jummbotronTop";
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
 			<NAV></NAV>
+			<JumTop></JumTop>
 		</div>
 	);
 };
---
+++ src/react_doc/jummbotronTop.js
@@ -0,0 +1,23 @@
+//import react into the bundle
+import React from "react";
+
+const JumTop = () => {
+    return(
+        <>
+        <div className="d-flex flex-column align-items-start p-5 m-4 text-muted  bg-body-tertiary border border-dashed rounded-5">
+    
+   
+    <h1 className="text-dark">A Warm Welcome</h1>
+    <p className="col-lg-6  mb-4 text-start">
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur adipisci nulla officia blanditiis nihil illum dolorum magnam dolor? Dolorum nulla ab blanditiis quis. Dolorem nulla commodi perferendis fugiat distinctio! Natus.
+
+    </p>
+    <button className="btn btn-primary px-5 mb-5" type="button">
+      Call to action
+    </button>
+  </div>
+        </>
+    )
+}
+
+export default JumTop
